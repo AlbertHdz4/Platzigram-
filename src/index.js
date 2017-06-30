fdbd6d689f279a3324cc9c1546341f2f4e6cf81b
Alberto Hernández <alberto.hernandez.l@outlook.com>
2017-06-29 19:36:10 -0500

    Se han solucionado los errores del servidor

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ var page = require('page');
 var main = document.getElementById('main-container');
 
 page('/', function(ctx, next) {
-  main.innerHTML = 'Home <a href="/signup">Signup</a> Sign Out Otro Link'; //diga home
+  main.innerHTML = 'Home <a href="/signup">Signup</a>'; //diga home
 })
 
 page('/signup', function(ctx, next) {
-  main.innerHTML = '<a href="/">Home</a> Sign Up Sign Out hola coomo estas '; //diga home
+  main.innerHTML = '<a href="/">Home</a> Otro enlace'; //diga home
 })
 
 page.start()
