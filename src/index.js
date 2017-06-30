72904f7880e08bcb24629e6caa234a3030f3c8ca
Alberto Hernández <alberto.hernandez.l@outlook.com>
2017-06-29 22:04:24 -0500

    Modularización de las paginas Homepage & Signup corregidas

--- src/index.js
+++ src/index.js
@@ -2,58 +2,8 @@
 // este es el encargado de juntar todas las dependencias de desarrollo
 // y otorgarlas a la interfaz que las necesite
 var page = require('page');
-var yoyo = require('yo-yo');
-var empty = require('empty-element');
+require('./homepage');
+require('./signup');
 
-var main = document.getElementById('main-container');
-
-page('/', function(ctx, next) {
-  main.innerHTML = "<a href='/signup'>Signup</a>";
-});
-
-page('/signup', function(ctx, next) {
-  var element = yoyo`<div class="container">
-        <div class="row">
-          <div class="col s10 push-s1">
-            <div class="row">
-              <!-- m5 se refiere a la anchura de columnas que tendra -->
-              <div class="col m5 hide-on-small-only">
-                <!-- imagen -->
-                <img src="iphone.png" class="iphone" alt="">
-              </div>
-              <div class="col s12 m7">
-                <div class="row">
-                  <div class="signup-box">
-                    <h1 class="platzigram">Platzigram</h1>
-                    <form class="signup-form">
-                      <h2>Registrate para ver fotos de tus amigos estudiando en Platzi</h2>
-                      <div class="section">
-                        <!-- Clases que ocultan los botones ya sea en escritorio o en moviles -->
-                        <a href="" class="btn btn-fb hide-on-small-only">Iniciar Sesión con Facebook</a>
-                        <a href="" class="btn btn-fb hide-on-med-and-up">Iniciar Sesión</a>
-                      </div>
-                      <div class="divider"></div>
-                      <div class="secti">
-                        <input type="email" name="email" value="" placeholder="Correo Electrónico">
-                        <input type="text" name="name" value="" placeholder="Nombre Completo">
-                        <input type="text" name="username" value="" placeholder="Nombre de Usuario">
-                        <input type="password" name="password" value="" placeholder="Contraseña">
-                        <button type="submit" class="btn waves-effect waves-light btn-signup">Regístrate</button>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="login-box">
-                    ¿Tienes una cuenta? <a href="/signin">Entrar</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>`;
-      main.appendChild(element);
-});
-
+// console.log(page());
 page();
